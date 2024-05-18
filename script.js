4f91c4bd9500ef0b6abd46e84e5d1ac2ad56a6ba
kirill-mrotzek <163567376+kirill-mrotzek@users.noreply.github.com>
2024-05-18 22:30:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let number1 = +prompt('Enter number1');
+console.log(number1 ** 3);
+
+let number2 = +prompt('Enter number2');
+let number3 = +prompt('Enter number3');
+console.log(number2 + number3);
+
+let username = prompt('Enter your name');
+let usersurname = prompt('Enter your surname');
+let userage = +prompt('Enter your age');
+let userposition = prompt('Enter your position');
+
+console.log(`Hi, my name is ${username} ${usersurname}. I was born in ${2024 - userage}. I'm ${userposition}.`)
